383fcb10342a5a7a702bfdeae53579fdf9a3166d
Brian Misachi <misachibrian@gmail.com>
2025-08-05 21:10:32 +0300

    Add trivial `psql`-like client for interacting with the server

---
+++ examples/psql.rs
@@ -0,0 +1,76 @@
+use bytes::{BufMut, BytesMut};
+use std::io::{self, Write};
+use std::net::Ipv4Addr;
+use std::str::FromStr;
+
+use pg_rusted_wire::wire::*;
+
+const DEFAULT_USER: &str = "postgres";
+const DEFAULT_PORT: u16 = 5432;
+const DEFAULT_IP: &str = "127.0.0.1";
+const DEFAULT_PASS: &str = "pass12234";
+
+fn main() {
+    let mut startup_msg = StartupMsg::new(
+        String::from(DEFAULT_USER),
+        Some(String::from(DEFAULT_USER)),
+        None,
+        None,
+    );
+
+    let client = Client::new(
+        Ipv4Addr::from_str(DEFAULT_IP).expect("IPV4 address error"),
+        DEFAULT_PORT,
+    );
+
+    match client.connect() {
+        Ok(mut stream) => {
+            if let Err(e) = client.authenticate(&mut stream, &mut startup_msg, DEFAULT_PASS) {
+                eprintln!("Client Authentication: {}", e);
+                return;
+            }
+
+            println!("Client connection\nUse Ctrl+c to end\n");
+
+            let mut result_buf = BytesMut::new();
+            let mut row_descr = BytesMut::new();
+
+            loop {
+                // Okay to use stream to send queries at this point
+                print!("psql> ");
+                let _ = io::stdout().flush(); // Flush immediately so we can have query in single line
+                let mut msg = String::new();
+
+                io::stdin().read_line(&mut msg).expect("Error in read line");
+
+                if msg.len() <= 1 {
+                    // Ignore empty. Account for newline char
+                    continue;
+                }
+
+                if let Err(e) =
+                    process_simple_query(&mut stream, &msg, &mut result_buf, &mut row_descr)
+                {
+                    eprintln!("Error processing simple query: {}", e);
+                    return;
+                }
+
+                result_buf.put_u8(b'\n');
+                row_descr.put_u8(b'\n');
+                if let Err(e) = io::stdout().write_all(&row_descr) {
+                    eprintln!("Error when writing to stdout for RowDescription: {}", e);
+                }
+                if let Err(e) = io::stdout().write_all(&result_buf) {
+                    eprintln!("Error when writing to stdout for DataRow: {}", e);
+                }
+
+                result_buf.clear();
+                row_descr.clear();
+            }
+        }
+        Err(e) => {
+            eprintln!("No stream available for client: {}", e);
+            return;
+        }
+    }
+}
